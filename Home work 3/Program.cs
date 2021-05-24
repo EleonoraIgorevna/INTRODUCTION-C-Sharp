6ea452df2bff04d3eefb5e1a770f93081e6e613d
[EleonoraPogorelova] <[pogorelova.eva@bk.ru]>
2021-05-24 23:23:11 +0300

    added some code

--- Home work 3/Program.cs
+++ Home work 3/Program.cs
@@ -6,17 +6,101 @@ namespace Home_work_3
     {
         static void Main(string[] args)
         {
-           /*Написать программу, выводящую элементы двухмерного массива по диагонали.
 
 
-           //Написать программу — телефонный справочник — создать двумерный массив 5*2, хранящий список телефонных контактов: 
-           // первый элемент хранит имя контакта, второй — номер телефона/e-mail.
+            // 1 . Написать программу, выводящую элементы двухмерного массива по диагонали.
+            /*
+            int[,] array = new int[5, 5];
+            array[0, 0] = 1;
+            array[1, 1] = 1;
+            array[2, 2] = 1;
+            array[3, 3] = 1;
+            array[4, 4] = 1;
+            for (int i = 0; i < array.GetLength(0); i++) 
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write($"{array[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+            //2.Написать программу — телефонный справочник — создать двумерный массив 5 * 2, хранящий список телефонных контактов: первый элемент хранит имя контакта,
+				// второй — номер телефона/ e - mail.
+            string[,] myphonebook = new string[,]
+            {
+                {"Griboedov", "+79158622325, Griboedov@mail.ru"},
+                {"Pushkin", "+79158622765, Pushkin@mail.ru"},
+                {"Tolstoi", "+79708622775, Tolstoi@mail.ru"},
+                {"Goncharov", "+79998622377, Goncharov@mail.ru"},
+                {"Ostrovskii", "+79778622111, Ostrovskii@mail.ru"}
+            };
+            for (int i = 0; i < myphonebook.GetLength(0); i++)
+            {
+                for (int j = 0; j < myphonebook.GetLength(1); j++)
+                {
+                   
+                    Console.Write($"{myphonebook[i, j]} ");
+                }
+                System.Console.WriteLine();
+            }
+            
+            //3. Написать программу, выводящую введененную пользователем строку в обратном порядке. Hello
+            char[] hello = { 'h', 'e', 'l', 'l', 'o' };
+            for (int x = hello.Length -1; x >=0; x--) 
+            {
+                
+                   Console.Write($"{hello[x]}");
+                
+            }*/
+
+            // 4 . Морской бой.
 
 
-           //Написать программу, выводящую введенную пользователем строку в обратном порядке (olleH вместо Hello).
+            char[,] array = new char[10, 10];
+
+            array[0, 0] = '0';
+            array[0, 1] = '1';
+            array[0, 2] = '2';
+            array[0, 3] = '3';
+            array[0, 4] = '4';
+            array[0, 5] = '5';
+            array[0, 6] = '6';
+            array[0, 7] = '7';
+            array[0, 8] = '8';
+            array[0, 9] = '9';
+
+            array[1, 0] = '1';
+            array[2, 0] = '2';
+            array[3, 0] = '3';
+            array[4, 0] = '4';
+            array[5, 0] = '5';
+            array[6, 0] = '6';
+            array[7, 0] = '7';
+            array[8, 0] = '8';
+            array[9, 0] = '9';
+
+            array[4, 4] = 'Х'; //корабль 1
+            array[5, 4] = 'Х';
+            array[6, 4] = 'Х';
+            array[7, 4] = 'Х';
+            array[7, 8] = 'Х'; //корабль 2
+            array[8, 8] = 'Х';
 
 
-           //«Морской бой» — вывести на экран массив 10х10, состоящий из символов X и O, где Х — элементы кораблей, а О — свободные клетки.*/
+            //не знаю, как пустые клетки отметить нулем ((
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write($"{array[i, j]} ");
+                }
+                Console.WriteLine();
+
+
+
+            }
+
 
         }
     }
---
+++ Homework 4/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Homework_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            //Написать метод GetFullName(string firstName, string lastName, string patronymic), принимающий на вход ФИО в разных аргументах и возвращающий объединённую строку с ФИО.
+            //Используя метод, написать программу, выводящую в консоль 3–4 разных ФИО.
+
+            /*
+            static void Main(string[] args)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    Console.WriteLine("Какая у тебя фамилия?");
+                    string FirstName = Console.ReadLine();
+                    Console.WriteLine("Какое имя.");
+                    string LastName = Console.ReadLine();
+                    Console.WriteLine("Напиши отчество.");
+                    string Patronymic = Console.ReadLine();
+                    string FullName = GetFullName(FirstName, LastName, Patronymic);
+                    Console.WriteLine(FullName);
+                }
+            }
+            static string GetFullName(string FirstName, string LastName, string Patronymic)
+            {
+                string UserName =  FirstName +" "+ LastName + " " + Patronymic ;
+                return UserName; 
+            }
+            //2
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Введи числа.");
+                string value = Console.ReadLine();
+                int SumOfValue = GetSum(value);
+                Console.WriteLine(SumOfValue);
+                Console.ReadLine();
+            }
+            static int GetSum(string value)
+            {
+                string[] quantity = value.Split(' ');// пробел как разделитель
+                int SumOfValue = 0;
+                for (int i = 0; i < quantity.Length; i++)
+                {
+                    string quantityvalue = quantity[i];
+                    int quantityvalue01 = int.TryParse(quantityvalue, out int num)
+                        ? num
+                        : 0;
+                    SumOfValue = SumOfValue + quantityvalue01;
+                }
+                return SumOfValue;
+                */
+        }
+    }
+}
